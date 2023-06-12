e58309591e4592f8b8f87815f0f6aee21ca603bc
Gerard Cunningham <107518916+gerardlcunningham@users.noreply.github.com>
2023-06-12 12:23:35 -1000

    Add files via upload

---
+++ number_guesser.py
@@ -0,0 +1,34 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please enter a number larger than 0.")
+        quit()
+else:
+    print("Please enter something numerical.")
+    quit()
+
+random_number = random.randrange(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Please take a guess!... ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please enter something numerical.")
+        continue
+    if user_guess == random_number:
+        print("Wow! You actually got it!")
+        break
+    elif user_guess > random_number:
+            print("You are too high!")
+    else:
+            print("You are too low!")
+
+print("You got it in", guesses, "guesses.")
